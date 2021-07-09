ac7a8578ad745938dcdbf76483592446adf86901
nikita <Kropotov.N.A@sberbank.ru>
2021-07-09 12:33:36 +0300

    add example with DecisionTreeClassifier

---
+++ Coursera(introduction_to_machine_learning)/DecisionTree/DecisionTree.py
@@ -0,0 +1,24 @@
+from pandas import read_csv, DataFrame
+from sklearn.tree import DecisionTreeClassifier
+
+
+def isNaN(num):
+    return num != num
+
+
+data = read_csv('./resource/train.csv')
+print(data.columns)
+
+df = DataFrame([])
+df['Pclass'] = data['Pclass']
+df['Fare'] = data['Fare']
+df['Age'] = data['Age']
+df['Sex'] = data['Sex']
+
+df.loc[df['Sex'] == 'male', 'Sex'] = 1
+df.loc[df['Sex'] == 'female', 'Sex'] = 0
+df = df.drop(df.loc[isNaN(df['Age']), 'Age'].index, axis=0)
+
+clf = DecisionTreeClassifier(random_state=241)
+clf.fit(df.iloc[:, 1:4].to_numpy(), df['Pclass'].to_numpy())
+print(clf.feature_importances_)
